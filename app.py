a57da95f171395563be230eadaf5eb743feb1ef0
Firoz1402 <firozanjum537@gmail.com>
2022-09-19 22:14:17 +0530

    This is for Web App

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 import pickle
+
 from flask import Flask,request, app, jsonify,url_for,redirect, render_template,flash,session,escape
 import flask
 import numpy as np
@@ -25,5 +26,17 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The Predicted Price of the Given Plot/Residential Area is {}".format(output))
+
 if __name__== "__main__":
     app.run(debug=True)
+
+
+
